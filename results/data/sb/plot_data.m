9e19737e80ac194d28ea604cfdbd3e65b56aef7a
Martin Stølen <martstol@gmail.com>
2014-12-30 21:08:43 +0100

    ...

--- results/data/sb/plot_data.m
+++ results/data/sb/plot_data.m
@@ -41,10 +41,35 @@ loglog(petsc_cpu_gmres(1, :), petsc_cpu_gmres(2, :), "Color", "magenta", "LineWi
 loglog(petsc_gpu_cg(1, :), petsc_gpu_cg(2, :), "Color", "red", "LineWidth", 1);
 loglog(petsc_gpu_gmres(1, :), petsc_gpu_gmres(2, :), "Color", "blue", "LineWidth", 1);
 xlabel("Grid points", "FontSize", 12, "FontName", "Arial");
-set(gca,'XTickLabel',num2str(get(gca,'XTick').'))
+set(gca,'XTickLabel',num2str(get(gca,'XTick').'));
 ylabel("Execution time (seconds)", "FontSize", 12, "FontName", "Arial");
 legend("SOR (CUDA)", "PETSc CPU CG", "PETSc CPU GMRES", "PETSc GPU CG", "PETSc GPU GMRES",
 	"Location", "SouthOutside");
 hold off;
 print("exec_time_all.png");
+
+clf;
+
+speedup_cg = zeros(2, n);
+speedup_gmres = zeros(2, n);
+
+for i = 1:n
+	speedup_cg(1, i) = petsc_gpu_cg(1, i);
+	speedup_cg(2, i) = petsc_cpu_cg(2, i) / petsc_gpu_cg(2, i);
+	
+	speedup_gmres(1, i) = petsc_gpu_gmres(1, i);
+	speedup_gmres(2, i) = petsc_cpu_gmres(2, i) / petsc_gpu_gmres(2, i);
+end
+
+hold on;
+title("Solver Speedup", "FontSize", 12, "FontName", "Arial");
+loglog(speedup_cg(1, :), speedup_cg(2, :), "Color", "blue");
+loglog(speedup_gmres(1, :), speedup_gmres(2, :), "Color", "red");
+xlabel("Grid points", "FontSize", 12, "FontName", "Arial");
+set(gca,'XTickLabel',num2str(get(gca,'XTick').'));
+ylabel("Speedup", "FontSize", 12, "FontName", "Arial");
+set(gca,'YTickLabel',num2str(get(gca,'YTick').'));
+legend("CG", "GMRES", "Location", "SouthOutside");
+hold off;
+print("speedup_all.png");
 
