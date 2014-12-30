36e3a0be944d8da9c425ba53fc6d1e6a9c83349f
Martin Stølen <martstol@gmail.com>
2014-12-30 19:45:06 +0100

    ...

--- results/data/sb/plot_data.m
+++ results/data/sb/plot_data.m
@@ -35,30 +35,16 @@ end
 
 hold on;
 title("Solver Execution Time", "FontSize", 12, "FontName", "Arial");
-plot(cuda(1, :), cuda(2, :), "Color", "blue");
-plot(petsc_cpu_cg(1, :), petsc_cpu_cg(2, :), "Color", "green");
-plot(petsc_cpu_gmres(1, :), petsc_cpu_gmres(2, :), "Color", "red");
-plot(petsc_gpu_cg(1, :), petsc_gpu_cg(2, :), "Color", "magenta");
-plot(petsc_gpu_gmres(1, :), petsc_gpu_gmres(2, :), "Color", "black");
+loglog(cuda(1, :), cuda(2, :), "Color", "black", "LineWidth", 1);
+loglog(petsc_cpu_cg(1, :), petsc_cpu_cg(2, :), "Color", "green", "LineWidth", 1);
+loglog(petsc_cpu_gmres(1, :), petsc_cpu_gmres(2, :), "Color", "magenta", "LineWidth", 1);
+loglog(petsc_gpu_cg(1, :), petsc_gpu_cg(2, :), "Color", "red", "LineWidth", 1);
+loglog(petsc_gpu_gmres(1, :), petsc_gpu_gmres(2, :), "Color", "blue", "LineWidth", 1);
 xlabel("Grid points", "FontSize", 12, "FontName", "Arial");
 set(gca,'XTickLabel',num2str(get(gca,'XTick').'))
 ylabel("Execution time (seconds)", "FontSize", 12, "FontName", "Arial");
-legend("Cuda (old)", "PETSc CPU CG", "PETSc CPU GMRES", "PETSc GPU CG", "PETSc GPU GMRES",
+legend("SOR (CUDA)", "PETSc CPU CG", "PETSc CPU GMRES", "PETSc GPU CG", "PETSc GPU GMRES",
 	"Location", "SouthOutside");
 hold off;
 print("exec_time_all.png");
-
-clf;
-
-hold on;
-title("GPU Solver Execution Time", "FontSize", 12, "FontName", "Arial");
-plot(cuda(1, :), cuda(2, :), "Color", "blue");
-plot(petsc_gpu_cg(1, :), petsc_gpu_cg(2, :), "Color", "magenta");
-plot(petsc_gpu_gmres(1, :), petsc_gpu_gmres(2, :), "Color", "black");
-xlabel("Grid points", "FontSize", 12, "FontName", "Arial");
-set(gca,'XTickLabel',num2str(get(gca,'XTick').'))
-ylabel("Execution time (seconds)", "FontSize", 12, "FontName", "Arial");
-legend("Cuda (old)", "PETSc GPU CG", "PETSc GPU GMRES", "Location", "SouthOutside");
-hold off;
-print("exec_time_gpu.png");
 
