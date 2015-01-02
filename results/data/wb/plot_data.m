71c5cfca39f00e33ee58b1355fd257506a9e4f18
Martin Stølen <martstol@gmail.com>
2015-01-02 21:19:53 +0100

    ...

--- results/data/wb/plot_data.m
+++ results/data/wb/plot_data.m
@@ -25,13 +25,19 @@ end
 
 hold on;
 title("Wind Simulation Execution Time", "FontSize", 12, "FontName", "Arial");
+
 loglog(cuda(1, :), cuda(2, :), "Color", "blue", "LineWidth", 1);
 loglog(petsc_cpu(1, :), petsc_cpu(2, :), "Color", "green", "LineWidth", 1);
 loglog(petsc_gpu(1, :), petsc_gpu(2, :), "Color", "red", "LineWidth", 1);
+
 xlabel("Grid points", "FontSize", 12, "FontName", "Arial");
 set(gca,'XTickLabel',num2str(get(gca,'XTick').'));
+
 ylabel("Execution time (seconds)", "FontSize", 12, "FontName", "Arial");
+set(gca,'YTickLabel',num2str(get(gca,'YTick').'));
+
 legend("SOR (CUDA)", "PETSc CPU", "PETSc GPU", "Location", "SouthOutside");
+
 hold off;
 print("exec_time_all.png");
 
